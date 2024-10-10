f90b3de3f47c15e14451ff825fa119b4bc4ca336
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-10-10 14:01:36 -0400

    Added persistent data handling

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/rest-api/db"
 	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
@@ -17,7 +19,13 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the events. Try again later"})
+		return
+	}
+
 	context.JSON(http.StatusOK, events)
 }
 
@@ -33,7 +41,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save the event. Try again later"})
+		return
+	}
 	
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
