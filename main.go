920ef3e173d6eb9a093b96ff75c2cfbb5f1fc257
cheternal7890 <cesar.jimenez.btw@gmail.com>
2024-10-10 15:18:07 -0400

    Added get endpoint for a single event

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api/db"
 	"example.com/rest-api/models"
@@ -13,9 +14,29 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
+
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
+}
+
+func getEvent(context *gin.Context) {
+	eventID, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event. Try again later"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func getEvents(context *gin.Context) {
@@ -47,6 +68,6 @@ func createEvent(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save the event. Try again later"})
 		return
 	}
-	
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
